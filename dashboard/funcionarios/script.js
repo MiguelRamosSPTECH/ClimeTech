0a74a09c4957e95cc8bd507db5989391ba85dfca
MiguelRamosSPTECH <miguel.ramos@sptech.school>
2025-04-18 21:51:24 -0300

    Adição do modal de indice da home da dashboard + ajuste no tamanho dos gráficos e nos modais

--- dashboard/funcionarios/script.js
+++ dashboard/funcionarios/script.js
@@ -1,5 +1,10 @@
+var nameFuncionario = input_nome.value;
+var emailFuncionario = input_email.value;
+var senhaFuncionario = input_senha.value;
+var titleEdit = textRequisicao.innerHTML;
 
 function openCloseModal(tipoModal) {
+
     if(tipoModal == "delete") {
         if(modal_delete.classList == "hide" && fade.classList == "hide") {
             modal_delete.classList.remove("hide");
@@ -9,6 +14,19 @@ function openCloseModal(tipoModal) {
             fade.classList.add("hide");
         }
     } else {
+
+        if(tipoModal == "insert") {
+            textRequisicao.innerHTML = `Criando um perfil`
+            input_nome.value = ``
+            input_email.value = ``
+            input_senha.value = ``
+        } else {
+            textRequisicao.innerHTML = titleEdit
+            input_nome.value = nameFuncionario
+            input_email.value = emailFuncionario
+            input_senha.value = senhaFuncionario
+        }
+        
         if(modal_update.classList == "hide" && fade.classList == "hide") {
             modal_update.classList.remove("hide");
             fade.classList.remove("hide");
@@ -16,5 +34,5 @@ function openCloseModal(tipoModal) {
             modal_update.classList.add("hide");
             fade.classList.add("hide");
         }
-    }
+    } 
 }
