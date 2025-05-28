ba0e8f41a7661cddb6538fe563b89328e2ceac22
Igor-Cardoso-SPTECH <igor.trindade@sptech.school>
2025-05-28 17:40:58 -0300

    corrigido mensagem cadastro

--- public/scripts/login_cadastro.js
+++ public/scripts/login_cadastro.js
@@ -5,13 +5,13 @@ function cadastrar() {
     let cnpjCadastro = ipt_cadastro_cnpj.value
     let emailCadastro = ipt_cadastro_email.value
     let senhaCadastro = ipt_cadastro_senha.value
-    let mensagem = `` 
+    let mensagem = ``
 
     if (cnpjCadastro.length != 14) {
         mensagem = `CNPJ deve conter 14 caracteres`
     } else if (!emailCadastro.includes("@") || !emailCadastro.includes(".")) {
         mensagem = `Email deve conter @ e .`
-    } else if(nomeCadastro == "" || cnpjCadastro == "" || emailCadastro == "" || senhaCadastro == "") {
+    } else if (nomeCadastro == "" || cnpjCadastro == "" || emailCadastro == "" || senhaCadastro == "") {
         mensagem = `Você deve preencher todos os campos do cadastro `
     } else {
         fetch("/empresas/cadastrar", {
@@ -26,19 +26,22 @@ function cadastrar() {
                 senhaEmpresa: senhaCadastro
             }),
         })
-        .then(async function (resposta) {
-            if(resposta.ok) {
-                alert("Cadastro efetuado com sucesso! redirecionando para o login")
-                setInterval(() => window.location = "../site/login.html", 2000);
-                //se quiser limpar form fodasse
-            } else {
-                mensagem = await resposta.text();
-                div_notificacao_cadastro.innerHTML = `${mensagem}`;
-            }
-        })
-        .catch(function (resposta) {
-            console.log("#ERRO", resposta)
-        })
+            .then(async function (resposta) {
+                if (resposta.ok) {
+                    cardErro.style.display = "block";
+                    setTimeout(() => {
+                        sumirMensagem();
+                        window.location = "../site/login.html";
+                    }, 2000);
+
+                } else {
+                    mensagem = await resposta.text();
+                    div_notificacao_cadastro.innerHTML = `${mensagem}`;
+                }
+            })
+            .catch(function (resposta) {
+                console.log("#ERRO", resposta)
+            })
     }
     div_notificacao_cadastro.innerHTML = `${mensagem}`;
 }
@@ -47,7 +50,7 @@ function login() {
     var emailLogin = ipt_login_email.value;
     var senhaLogin = ipt_login_senha.value;
     let mensagem = ``
-    if(emailLogin == "" || senhaLogin == "") {
+    if (emailLogin == "" || senhaLogin == "") {
         mensagem = `Preencha todos os campos!`
     } else {
         fetch("/usuarios/autenticar", {
@@ -60,26 +63,26 @@ function login() {
                 senhaEmpresa: senhaLogin
             }),
         })
-        .then(async function (resposta) {
-            if(resposta.ok) {
-                const dadosUsuario = await resposta.json();
-                // if(dadosUsuario[0].tipoAcesso != "admin") {
+            .then(async function (resposta) {
+                if (resposta.ok) {
+                    const dadosUsuario = await resposta.json();
+                    // if(dadosUsuario[0].tipoAcesso != "admin") {
                     sessionStorage.EMAIL_FUNCIONARIO = dadosUsuario[0].email;
                     sessionStorage.SENHA_FUNCIONARIO = dadosUsuario[0].senha;
                     sessionStorage.ACESSO_FUNCIONARIO = dadosUsuario[0].tipoAcesso;
                     sessionStorage.ID_EMPRESA = dadosUsuario[0].empresaId;
                     window.location = "../dashboard/index.html";
-                // } 
+                    // } 
 
-            } else {
-                const msgErro = await resposta.text();
-                div_notificacao_login.innerHTML = `${msgErro}`
-            }
-        })
+                } else {
+                    const msgErro = await resposta.text();
+                    div_notificacao_login.innerHTML = `${msgErro}`
+                }
+            })
     }
     div_notificacao_login.innerHTML = `${mensagem}`
 }
 
- function sumirMensagem() {
+function sumirMensagem() {
     cardErro.style.display = "none";
-  }
+}
