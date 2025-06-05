eeee47ebf51f1c1c0fbb7123c4817a62b1c04a48
VictorDuarte-SPTECH <victor.duarte@sptech.school>
2025-06-05 14:54:22 -0300

    Update usuarioModel.js reorganizando as queries

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,8 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idFuncionarioEmpresa, fe.nome, fe.email, fe.senha, fe.idEmpresa as empresaId, tipoAcesso FROM funcionarioEmpresa fe
+        SELECT idFuncionarioEmpresa, fe.nome, fe.email, fe.senha, fe.idEmpresa as empresaId, tipoAcesso
+        FROM funcionarioEmpresa fe
         INNER JOIN empresa e ON
         fe.idEmpresa = e.idEmpresa
         WHERE fe.email = '${email}' AND fe.senha = '${senha}';
@@ -17,9 +18,10 @@ function cadastrar(nome, email, senha, acesso, fkEmpresa) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
 
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+    // e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO funcionarioEmpresa (nome, email, senha, tipoAcesso, idEmpresa, dtCriacao) VALUES ('${nome}', '${email}', '${senha}', '${acesso}', ${fkEmpresa}, curdate());
+        INSERT INTO funcionarioEmpresa (nome, email, senha, tipoAcesso, idEmpresa, dtCriacao) 
+        VALUES ('${nome}', '${email}', '${senha}', '${acesso}', ${fkEmpresa}, curdate());
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -27,7 +29,11 @@ function cadastrar(nome, email, senha, acesso, fkEmpresa) {
 
 
 function selectAllFuncionarios(idEmpresa) {
-    var instrucaoSql = `SELECT idFuncionarioEmpresa, nome, email, senha, tipoAcesso, dtCriacao FROM funcionarioEmpresa WHERE idEmpresa = ${idEmpresa};`
+    var instrucaoSql = `
+        SELECT idFuncionarioEmpresa, nome, email, senha, tipoAcesso, dtCriacao
+        FROM funcionarioEmpresa
+        WHERE idEmpresa = ${idEmpresa};
+        `;
 
     console.log("Exeucutando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -38,13 +44,14 @@ function editarFuncionario(nomeFuncionario, emailFuncionario, senhaFuncionario, 
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editarFuncionario():", nomeFuncionario, emailFuncionario, senhaFuncionario, acessoFuncionario, idFuncionario);
 
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+    // e na ordem de inserção dos dados.
     var instrucaoSql = `
         UPDATE funcionarioEmpresa 
         SET 
         nome = '${nomeFuncionario}', 
         email = '${emailFuncionario}', 
-        senha = '${senhaFuncionario}', tipoAcesso = '${acessoFuncionario}' 
+        senha = '${senhaFuncionario}',
+        tipoAcesso = '${acessoFuncionario}' 
         where idFuncionarioEmpresa = ${idFuncionario};
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -52,8 +59,10 @@ function editarFuncionario(nomeFuncionario, emailFuncionario, senhaFuncionario, 
 }
 
 
-function deletarFuncionario(idFuncionario){
-var instrucaoSql = ` delete from funcionarioEmpresa where idFuncionarioEmpresa = ${idFuncionario};`
+function deletarFuncionario(idFuncionario) {
+    var instrucaoSql = `
+        DELETE FROM funcionarioEmpresa WHERE idFuncionarioEmpresa = ${idFuncionario};
+    `;
 
     console.log("Exeucutando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -62,7 +71,10 @@ var instrucaoSql = ` delete from funcionarioEmpresa where idFuncionarioEmpresa =
 }
 
 function listarFuncionarioUpdate(idFuncionario) {
-    var instrucaoSql = `SELECT idFuncionarioEmpresa, nome, email, senha, tipoAcesso, dtCriacao FROM funcionarioEmpresa WHERE idFuncionarioEmpresa = ${idFuncionario};`
+    var instrucaoSql = `
+        SELECT idFuncionarioEmpresa, nome, email, senha, tipoAcesso, dtCriacao
+        FROM funcionarioEmpresa WHERE idFuncionarioEmpresa = ${idFuncionario};
+    `;
 
     console.log("Exeucutando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
